e24df5df0fbfe8c3cbba2a24047c34e3e9c9f6d1
Kazuki Inamura <kzinmr109@gmail.com>
2020-11-12 20:09:35 +0900

    fix: span prediction

--- app.py
+++ app.py
@@ -13,6 +13,34 @@ def jsonl_ja(js):
     return json.dumps(js, ensure_ascii=False)
 
 
+def convert_spans(jsons):
+    jsons_new = []
+    for j in jsons:
+        text = j["text"]
+        labels_all = []
+        for ent in j["persons"]:
+            if len(ent) > 2:
+                pattern = re.compile("(?:%s){i<=3:\s}" % (re.escape(ent)))
+                spans = [m.span() for m in pattern.finditer(text)]
+                labels = [[s, e, "PERSON"] for s, e in spans]
+                labels_all.extend(labels)
+        for ent in j["companies"]:
+            if len(ent) > 2:
+                pattern = re.compile("(?:%s){i<=3:\s}" % (re.escape(ent)))
+                spans = [m.span() for m in pattern.finditer(text)]
+                labels = [[s, e, "COMPANY"] for s, e in spans]
+                labels_all.extend(labels)
+        for ent in j["addresses"]:
+            if len(ent) > 2:
+                pattern = re.compile("(?:%s){i<=3:\s}" % (re.escape(ent)))
+                spans = [m.span() for m in pattern.finditer(text)]
+                labels = [[s, e, "ADDRESS"] for s, e in spans]
+                labels_all.extend(labels)
+        meta = {"doc_id": j["doc_id"], "annotations": j["gold"]}
+        jsons_new.append({"text": text, "labels": labels_all, "meta": meta})
+    return jsons_new
+
+
 if __name__ == "__main__":
     with jsonlines.open("/app/data/text.jsonl") as fp:
         jsons = [jsonstr for jsonstr in fp.iter()]
@@ -35,4 +63,8 @@ if __name__ == "__main__":
         outputs.append(output)
     with jsonlines.open("/app/data/predict.jsonl", "w", dumps=jsonl_ja) as fp:
         fp.write_all(outputs)
-    print(output)
+    span_jsons = convert_spans(outputs)
+    with jsonlines.open("/app/data/predict_spans.jsonl", "w", dumps=jsonl_ja) as fp:
+        fp.write_all(span_jsons)
+    print(output[-1])
+    print(span_jsons[-1])
