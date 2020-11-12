aa8eb309a88e115c830617604d49c2c23fef09c6
Kazuki Inamura <kzinmr109@gmail.com>
2020-11-12 19:42:44 +0900

    fix: rm space for person names

--- person_matcher.py
+++ person_matcher.py
@@ -1,3 +1,4 @@
+import regex as re
 from mecab_matcher import MecabMatcher
 
 
@@ -5,5 +6,6 @@ class PersonMatcher(MecabMatcher):
     def __init__(self):
         super().__init__("名詞-固有名詞-人名")
 
-    # def parse(self, text: str):
-    #     return self.parse_tokens(text)
+    def parse(self, text: str):
+        text = re.sub("\s", "", text)
+        return super().parse(text)
